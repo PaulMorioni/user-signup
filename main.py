70d0c1607a4a0c137ef0d3e95a64a108c431db51
PaulMorioni <44041460+PaulMorioni@users.noreply.github.com>
2019-02-13 19:51:50 -0600

    Persistent form data

--- main.py
+++ main.py
@@ -66,6 +66,7 @@ def validate_user():
 @app.route('/valid')
 def valid_user():
     username = request.args.get('username')
+    password = request
     template = jinja_env.get_template('user_greeting.html')
     return template.render(username = username)
 
